32588f9ba11632b01a45f236fa778d3be13914ff
Zhang Zhenchuan <kmusoy@gmail.com>
2019-02-28 23:17:59 +0800

    添加pat_1061。没难度，就是考细心和阅读理解，没有回味价值

--- pat_1061/pat_1061.cc
+++ pat_1061/pat_1061.cc
@@ -1,3 +1,5 @@
+
+// 没难度，就是考阅读理解和细心。没有回味价值
 #include <iostream>
 #include <stdio.h>
 #include <vector>
@@ -6,40 +8,37 @@ using namespace std;
 
 string weekRpr[] = {"MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"};
 
-bool isLetter(char cc)
+bool isWeek(char cc)
 {
-    return ((cc>='a'&&cc<='z')||(cc>='A'&&cc<='Z'));
+    return cc >= 'A' && cc <= 'G';
+}
+
+bool isHour(char cc)
+{
+    return (cc >= 'A' && cc <= 'N') || (cc >= '0' && cc <= '9');
+}
+
+bool isLower(char cc)
+{
+    return cc >= 'a' && cc <= 'z';
 }
 
 int main(int argc, char const *argv[])
 {
     string s0, s1, s2, s3;
-    char day='Z', hour='Z';
+    char day = 'Z', hour = 'Z';
     int minute;
     cin >> s0 >> s1 >> s2 >> s3;
-    for(int ii=0;ii<min(s0.size(), s1.size())&&hour=='Z';ii++)
-    {
-        if (s0[ii]!=s1[ii]) continue;
-        if (!isLetter(s0[ii])) continue;
-        if (day=='Z')
-        {
-            day = s0[ii];
-        }
-        else{
-            hour = s0[ii];
-        }
-    }
-    for(int ii=0;ii<min(s2.size(), s3.size());ii++)
-    {
-        if (!isLetter(s2[ii])) continue;
-        if (s2[ii]==s3[ii])
-        {
-            minute = ii;
-            break;
-        }
-    }
-    cout << weekRpr[day-'A'] << " " << ((isLetter(hour))?hour-'A'+10:hour-'0') << ":";
-    printf("%02d\n", minute);
+    int ii = 0;
+    for (; s0[ii] != s1[ii] || !isWeek(s0[ii]); ii++)
+        ;
+    day = s0[ii++];
+    for (; s0[ii] != s1[ii] || !isHour(s0[ii]); ii++)
+        ;
+    hour = s0[ii];
+    for (ii = 0; s2[ii] != s3[ii] || !isalpha(s2[ii]); ii++) //这里题目很明确的说了是english letter,不知道为什么自己之前还是理解成了小写字母
+        ;
+    minute = ii;
+    printf("%s %02d:%02d\n", weekRpr[day - 'A'].c_str(), (hour >= 'A' && hour <= 'N' ? hour - 'A' + 10 : hour - '0'), minute);
     return 0;
 }
-
