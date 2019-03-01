e14a27f8d1de2f2965bc9460262257564e83d91f
Zhang Zhenchuan <kmusoy@gmail.com>
2019-03-01 11:48:23 +0800

    添加pat_1150。非常简单，没有坑，之前没写对，只是自己傻逼，连读取都能出问题

--- pat_1150/pat_1150.cc
+++ pat_1150/pat_1150.cc
@@ -1,6 +1,9 @@
+// 没难度，如果考试出现这种题，最多半小时内肯定要搞定。
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <set>
 using namespace std;
 
 int nn, mm;
@@ -9,56 +12,64 @@ vector<vector<int>> edge;
 int main()
 {
     cin >> nn >> mm;
-    edge.resize(nn+1, vector<int>(nn+1, -1));
-    int st, ed, ll;
-    for(int ii=0;ii<mm;ii++)
+    edge.resize(nn + 1, vector<int>(nn + 1, -1));
+    int st, ed, ll, kk;
+    for (int ii = 0; ii < mm; ii++)
     {
         cin >> st >> ed >> ll;
         edge[st][ed] = edge[ed][st] = ll;
     }
-    int kk;
+
     cin >> kk;
-    int minDist=UINT16_MAX, minId=-1;
-    for(int ii=1;ii<=kk;ii++)
+    int minDist = UINT16_MAX, minId = -1;
+    for (int ii = 1; ii <= kk; ii++)
     {
         cout << "Path " << ii << ": ";
-        int nc, dist=0;
-        vector<bool> visited(nn+1, false);visited[0] = true;
+        int nc, dist = 0;
+        // vector<bool> visited(nn + 1, false);
+        set<int> visited;
         bool invalidPath = false;
+
         cin >> nc >> st;
         int st_0 = st;
 
-        if (st<=0||st>nn) invalidPath = true;
-        visited[st]=true;
+        if (st <= 0 || st > nn)
+            invalidPath = true;
+        visited.insert(st);
 
-        for(int jj=1;jj<nc&&!invalidPath;jj++)
+        for (int jj = 1; jj < nc; jj++) //原来这里出错了。不管什么情况，都要把这一串读完，不然会影响后面的。原来的失误也太低级了。
         {
             cin >> ed;
-            if (ed<=0||ed>nn||edge[st][ed]==-1) {
+            if (ed <= 0 || ed > nn || edge[st][ed] == -1)
+            {
                 invalidPath = true;
             }
-            else{
+            else
+            {
                 dist += edge[st][ed];
-                visited[ed] = true;
-                st = ed;
+                visited.insert(ed);
             }
+            st = ed;
         }
 
-        if (invalidPath) {
-            cout << "NA (Not a TS cycle)" << endl; 
+        if (invalidPath)
+        {
+            cout << "NA (Not a TS cycle)" << endl;
             continue;
         }
-        if (st_0==ed&&find(visited.begin(), visited.end(), false)==visited.end())
+        if (st_0 == ed && visited.size()==nn)   //始末相同，且访问过所有结点
         {
-            if (dist<minDist)
+            if (dist < minDist)
             {
                 minDist = dist;
                 minId = ii;
             }
-            if (nc==nn+1) {
+            if (nc == nn + 1)                   //cycle
+            {
                 cout << dist << " (TS simple cycle)" << endl;
             }
-            else{
+            else                                // there's some city been visited more than once 
+            {
                 cout << dist << " (TS cycle)" << endl;
             }
         }
@@ -67,6 +78,6 @@ int main()
             cout << dist << " (Not a TS cycle)" << endl;
         }
     }
-    cout << "Shortest Dist("<< minId <<") = " << minDist << endl;
+    cout << "Shortest Dist(" << minId << ") = " << minDist << endl;
     return 0;
 }
