92721504fe350223a7013f0760a580ae0eeebd26
Zhang Zhenchuan <kmusoy@gmail.com>
2019-03-01 22:22:45 +0800

    添加pat_1112。简单送分题，但是自己粗心写错边界和输出，之前一直卡着。没什么价值

--- pat_1112/pat_1112.cc
+++ pat_1112/pat_1112.cc
@@ -1,26 +1,33 @@
+// 还是因为粗心造成之前没能AC。
+// 一是边界写错
+// 二是输出顺序有误
+
 #include <iostream>
 #include <stdio.h>
 #include <string>
 #include <vector>
+#include <set>
 using namespace std;
 
 bool isStucked[256];
 string line;
+set<char> normalKeys;
+set<char> stuckKeys;
 
 int main()
 {
     int kk;
     cin >> kk;
     cin >> line;
-    fill(isStucked, isStucked+256, false);
-    int ii=0;
-    for(int ii=0;ii<line.size()-kk;)
+    fill(isStucked, isStucked + 256, false);
+    int ii = 0;
+    for (int ii = 0; ii <= line.size() - kk;) //原来这里的结束条件是<而不是<=，这种小细节，一定要在写的时候就发现，找起来太麻烦了
     // while(ii<line.size()-kk)
     {
         bool isEqual = true;
-        for(int jj=ii+1;jj<ii+kk;jj++)
+        for (int jj = ii + 1; jj < ii + kk; jj++)
         {
-            if (line[jj]!=line[ii])
+            if (line[jj] != line[ii])
             {
                 isEqual = false;
                 break;
@@ -29,52 +36,33 @@ int main()
         if (isEqual)
         {
             isStucked[line[ii]] = true;
-            ii+=kk;
+            stuckKeys.insert(line[ii]);
+            ii += kk;
         }
         else
         {
+            normalKeys.insert(line[ii]);
             ii++;
         }
     }
-    for(int ii=0;ii<line.size()-1;)
-    {
-        if (!isStucked[line[ii]]) 
+    for (auto cc : stuckKeys) //修改了排除误认为是stuckedkey的那些情况，就是在别的地方是好的，逻辑上和原来没变
+        if (normalKeys.find(cc) != normalKeys.end())
         {
-            ii++;
-            continue;
+            stuckKeys.erase(cc);
+            isStucked[cc] = false;
         }
-        bool isEqual = true;
-        for(int jj=ii+1;jj<ii+kk&&jj<line.size();jj++)
+    for (int ii = 0; ii < line.size(); ii++)
+        if (stuckKeys.find(line[ii]) != stuckKeys.end())
         {
-            if (line[jj]!=line[ii])
-            {
-                isEqual = false;
-                break;
-            }
+            printf("%c", line[ii]);
+            stuckKeys.erase(line[ii]);
         }
-        if (isEqual){
-            ii+=kk;
-        }
-        else
-        {
-            isStucked[line[ii]] = false;
-        }
-    }
-    for(int ii=0;ii<256;ii++)
-    if (isStucked[ii]) printf("%c", ii);
-    cout << endl;
-    for(int ii=0;ii<line.size();)
+    printf("\n");
+
+    for (int ii = 0; ii < line.size();)
     {
         printf("%c", line[ii]);
-        if (isStucked[line[ii]])
-        {
-            ii+=kk;
-        }
-        else
-        {
-            ii++;
-        }
+        ii += isStucked[line[ii]] ? kk : 1;
     }
-    cout << endl;
     return 0;
 }
